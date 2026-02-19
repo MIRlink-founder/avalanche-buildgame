e9e77da8dd1b3fe2fb15bf687a60fee59dbc1244
Hyeonjeong-JANG <Hyeonjeong-JANG@users.noreply.github.com>
2026-02-19 10:58:58 +0900

    feat: 공통 UI 컴포넌트와 아이콘 설정 추가

--- apps/hospital-web/app/layout.tsx
+++ apps/hospital-web/app/layout.tsx
@@ -1,35 +1,39 @@
-import type { Metadata } from "next"
-import { Geist, Geist_Mono } from "next/font/google"
-import { headers } from "next/headers"
-import { cookieToInitialState } from "wagmi"
-import { config } from "@mire/blockchain/wagmi/config"
-import { Providers } from "./providers"
-import "./globals.css"
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { headers } from 'next/headers';
+import { config as faConfig } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import { cookieToInitialState } from 'wagmi';
+import { config } from '@mire/blockchain/wagmi/config';
+import { Providers } from './providers';
+import './globals.css';
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-})
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-})
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+faConfig.autoAddCss = false;
 
 export const metadata: Metadata = {
-  title: "MI;Re", // TODO
-  description: "My Implant Record",
-}
+  title: 'MI;Re', // TODO
+  description: 'My Implant Record',
+};
 
 export default async function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   const initialState = cookieToInitialState(
     config,
-    (await headers()).get("cookie")
-  )
+    (await headers()).get('cookie'),
+  );
 
   return (
     <html lang="ko">
@@ -39,5 +43,5 @@ export default async function RootLayout({
         <Providers initialState={initialState}>{children}</Providers>
       </body>
     </html>
-  )
+  );
 }
